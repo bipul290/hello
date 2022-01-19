341eb85961a0f55f0d2707dea88aa9dae67d5383
bipul290 <90503084+bipul290@users.noreply.github.com>
2022-01-19 19:50:00 +0600

    Add files via upload

---
+++ my practise/js/custom.js
@@ -0,0 +1,22 @@
+// document.getElementById("okay").addEventListener("click", function(event){
+//     event.preventDefault()
+//   });
+
+
+
+function received_data() {
+    
+    let userName = document.getElementById("user").value;
+    let userPass = document.getElementById("pass").value;
+    
+    if(userName == "bipul" && userPass == 1234567){
+        
+    }
+    // else if(userPass.length <= 6 || userPass.length >= 12){
+    //     alert("morethen 6 character");
+    // }else{
+    //     alert("login Fail");
+    // }
+    
+
+}
